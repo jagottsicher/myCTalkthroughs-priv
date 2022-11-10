f657aad1f4d319f583804677614a222b26359477
Jens Schendel <30713344+jagottsicher@users.noreply.github.com>
2022-11-10 09:27:25 +0800

    Update temp_pointers.c

--- text/temp_pointers.c
+++ text/temp_pointers.c
@@ -1,46 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-void alterTheValue(int x);
-void alterTheValueByRef(int * ptr_b);
-
-int main()
-{
-    int x = 23;
-
-    alterTheValue(x);
-    alterTheValueByRef(&x);
-
-    int * ptr_x = &x;
-    int y = *ptr_x;
-
-    printf("%i\n", y);
-
-
-
-    return 0;
-}
-
-void alterTheValue(int a) {
-
-   a++;
-   printf("%i\n", a);
-
-}
-
-void alterTheValueByRef(int * ptr_b) {
-
-   *ptr_b = *ptr_b + 1;
-   printf("%i\n", *ptr_b);
-
-   printf("%x\n", ptr_b);
-   printf("%x\n", ++ptr_b);
-
-}
-
-
-#include <stdio.h>
-#include <stdlib.h>
 #include <typeinfo>
 
 void alterTheValue(int x);
@@ -79,4 +38,46 @@ void alterTheValueByRef(int * ptr_b) {
    printf("%s\n", typeid(ptr_b).name());
    printf("%x\n", ++ptr_b);
   
+}
+
+//////////////////////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void alterTheValue(int x);
+void alterTheValueByRef(int * ptr_b);
+
+int main()
+{
+    int x = 23;
+
+    alterTheValue(x);
+    alterTheValueByRef(&x);
+
+    int * ptr_x = &x;
+    int y = *ptr_x;
+
+    printf("%i\n", y);
+
+
+
+    return 0;
+}
+
+void alterTheValue(int a) {
+
+   a++;
+   printf("%i\n", a);
+
+}
+
+void alterTheValueByRef(int * ptr_b) {
+
+   *ptr_b = *ptr_b + 1;
+   printf("%i\n", *ptr_b);
+
+   printf("%x\n", ptr_b);
+   printf("%x\n", ++ptr_b);
+
 }
